5fdc16315ab1c0f62b7e2d338d6873368823cf26
Sneaky-Programmer <eshaan4172@gmail.com>
2020-10-28 12:25:30 +0530

    owofy added and default logo added

---
+++ src/functions/OWOfy.ts
@@ -0,0 +1,14 @@
+export default function OwOfy(v: string) {
+    v = v.replace(/(?:r|l)/g, "w");
+    v = v.replace(/(?:R|L)/g, "W");
+    v = v.replace(/n([aeiou])/g, "ny$1");
+    v = v.replace(/N([aeiou])/g, "Ny$1");
+    v = v.replace(/N([AEIOU])/g, "Ny$1");
+    v = v.replace(/ove/g, "uv");
+    const faces = ["(・`ω´・)", ";;w;;", "owo", "UwU", ">w<", "^w^"]
+    v = v.replace(
+      /\!+/g,
+      " " + faces[Math.floor(Math.random() * faces.length)] + " "
+    );
+    return v
+}
