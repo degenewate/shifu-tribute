7bf1f5efdfc8484d7708a18606623b9d708fd2a0
Sneaky-Programmer <eshaan4172@gmail.com>
2020-10-28 12:03:57 +0530

    init of a shitstorm

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,27 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+  <img alt="Vue logo" src="./assets/logo.png" />
+  <FooterVue />
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue';
-import HelloWorld from './components/HelloWorld.vue';
-
+import { defineComponent } from "vue";
+import FooterVue from "@/components/footer.vue";
 export default defineComponent({
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
+    FooterVue
   }
 });
 </script>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  @import "../node_modules/bulma/bulma.sass";
+  #app {
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
+  }
 </style>
---
+++ src/components/footer.vue
@@ -0,0 +1,22 @@
+<template>
+  <footer class="footer">
+    <div class="content box has-text-centered">
+      <p><strong>idk i need help</strong> by a dumbass.</p>
+      <a class="button mr-1 is-rounded" href="https://youtu.be/Ml2b9wXEe9s">
+        source code
+      </a>
+      <a
+        class="button ml-1 is-rounded"
+        href="https://github.com/Sneaky-Programmer/shifu-tribute"
+      >
+        just a rick roll dw
+      </a>
+    </div>
+  </footer>
+</template>
+<script lang="ts">
+import { defineComponent } from "vue";
+export default defineComponent({
+  name: "Footer"
+});
+</script>
